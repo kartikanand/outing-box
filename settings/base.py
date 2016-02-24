1728ea42bfd304e0d6d850312063623ed674aff5
Kartik Anand <kartikanand1992@gmail.com>
2016-02-24 23:49:06 +0530

    adding box header images

--- settings/base.py
+++ settings/base.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'debug_toolbar',
     'outingbox',
     'captcha',
     'crispy_forms',
